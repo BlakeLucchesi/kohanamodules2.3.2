78b45e66260a21f6942c338f15dc157682a780c7
Wouter <wouter.w@gmx.net>
2009-03-29 22:36:53 +0200

    firt commit

---
+++ a2-acl-demo/models/blog.php
@@ -0,0 +1,19 @@
+<?php
+
+class Blog_Model extends ORM implements Acl_Resource_Interface {
+
+	public function get_resource_id()
+	{
+		return 'blog';
+	}
+
+	public function validate(array & $array, $save = FALSE)
+	{
+		$array = Validation::factory($array)
+			->pre_filter('trim')
+			->add_rules('text','required');
+
+		return parent::validate($array, $save);
+	}
+
+} // End Blog Model
